3eb9f518d08710bd9116189202d0c7f351cbc4e0
Alisson Soares <alissongsoares@hotmail.com>
2026-06-06 08:27:44 -0300

    Continuando estudos sobre classes e métodos. aulasteoricas.aula25 apagado e reconstruído para fixação do aprendizado.

---
+++ ws-eclipse/programacao-orientada-objetos/src/aulasteoricas/aula25/TesteCarro.java
@@ -0,0 +1,20 @@
+package aulasteoricas.aula25;
+
+public class TesteCarro {
+	
+	public static void main(String[] args) {
+		
+		Carro van = new Carro();
+		van.marca = "Fiat";
+		van.modelo = "Ducato";
+		van.numPassageiros = 12;
+		van.capCombustivel = 60;
+		van.consumoCombustivel = 8;
+		
+		van.exibirAutonomia();
+		
+		
+		
+	}
+
+}
